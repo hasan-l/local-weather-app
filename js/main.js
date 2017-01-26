30cea69933314d611818f5a986f2fa57e33d4b10
Hasan Liaquat <hasan.liaquat@hotmail.com>
2017-01-26 05:41:29 +0000

    changed weather API and made improvements

--- js/main.js
+++ js/main.js
@@ -1,85 +1,49 @@
 $(document).ready(function(){
-	
-	$('#expand-btn').click(expandDiv);
 
 	function expandDiv(){
 		$('.background-image').toggleClass('changed');
 		$('.main').toggleClass('main-changed');
 		$('#logo').toggleClass('hidden');
-		$('#info-expanded').toggleClass('hidden');
-		$('#weather-desc').toggleClass('hidden');
+		$('.credits').toggleClass('hidden');
+		$('#weather-icon').toggleClass('hidden');
 
+		//set text to minimise or expand
+		var originalText = $('#expand-btn').text().trim();
 
-        var originalText = $(this).text().trim();
-
-        $("#expand-btn").text("Minimise");
-
-        if (originalText == "Minimise") {
-            $(this).text("Expand");
+		if(originalText === 'Expand'){
+			$('#expand-btn').text('Minimise');
+			return;
+		} else {
+            $('#expand-btn').text('Expand');
         }
-	}
+	}//expandDiv
 
+	$('#expand-btn').click(expandDiv);
 
-
-	//get city location
-	$.getJSON("https://freegeoip.net/json/", getUserLocation);
+	//get user location
+	$.getJSON("http://freegeoip.net/json/", getUserLocation);
 	var city, latitude, longitude;
 
 	function getUserLocation(data){
 		city = data.city;
 		latitude = data.latitude;
 		longitude = data.longitude;
-    	$('#city-name').html(city);
+    	$('#city-name').html(city + ", " + data.region_name);
 
-    	var openWeatherAPI = "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&APPID=7d5c5edd1c7087657932772fe42c8d39";
-		$.getJSON(openWeatherAPI, setWeatherData);
+
+    	var wundergroundAPI = 'https://api.wunderground.com/api/1fd8a0bdde10363b/geolookup/conditions/q/' + latitude + ',' + longitude + '.json';
+		$.getJSON(wundergroundAPI, setWeatherData);
 
 		//callback
 		function setWeatherData(json){
-			$('#temp-num').html(Math.round(json.main.temp));
-			$('#weather-main').html(json.weather[0].main);
+			$('#temp-num').html(Math.round(json.current_observation.temp_c));
+			$('#weather-main').html(json.current_observation.weather);
 
-			var backgroundImgUrl;
-			switch(json.weather[0].main){
-				case "Clouds":
-					backgroundImgUrl = "url(img/cloudy.jpg)";
-					$('#weather-icon').addClass(' wi-cloudy');
-					break;
-				case "Drizzle": 
-					backgroundImgUrl = "url(img/drizzle.jpeg)";
-					$('#weather-icon').addClass(' wi-showers');
-					break;
-				case "Rain": 
-					backgroundImgUrl = "url(img/rain.jpg)";
-					$('#weather-icon').addClass(' wi-rain');
-					break;
-				case "Snow": 
-					backgroundImgUrl = "url(img/snowy.jpg)";
-					$('#weather-icon').addClass(' wi-snow');
-					break;
-				case "Sunny":
-					backgroundImgUrl = "url(img/sunny.jpg)";
-					$('#weather-icon').addClass(' wi-hot');
-					break;
-				case "Clear":
-					backgroundImgUrl = "url(img/clear.jpg)";
-					$('#weather-icon').addClass(' wi-night-clear');
-					break;
-				case "Thunderstorm":
-					backgroundImgUrl = "url(img/thunderstorm.jpg)";
-					$('#weather-icon').addClass(' wi-thunderstorm');
-					break;
-				default:
-					backgroundImgUrl = "url(img/sunny.jpg)";
-					$('#weather-icon').addClass(' wi-hot');
-			}
-			$('.background-image').css('background-image', backgroundImgUrl);
-			$('#info-expanded').html((json.weather[0].description).toUpperCase());
-		
+			setBackground(json.current_observation.weather);			
 
 			var tempInCelsius = parseInt($('#temp-num').text(), 10);
 	
-			//change fron celsius to fahrenheit
+			//change fron celsius to fahrenheit and back
 			$('#temp-measure').click(function(){
 
 				var text = $(this).text().trim();
@@ -93,13 +57,43 @@ $(document).ready(function(){
 					$('#temp-num').html(tempInCelsius);
 					
 				}
-			});
+			});//temp-measure click
+		}//setWeatherData
+	}//getUserLocation
 
 
+	function setBackground(desc){
+
+		var backgroundImgUrl;
+
+		if(desc.includes("Cloud") || desc.includes("Overcast")){
+			backgroundImgUrl = "url(img/cloudy.jpg)";
+			$('#weather-icon').addClass(' wi-cloudy');
+		} else if(desc.includes("Clear")){
+			backgroundImgUrl = "url(img/clear.jpg)";
+			$('#weather-icon').addClass(' wi-night-clear');
+		} else if(desc.includes("Rain") || desc.includes("Spray") || desc.includes("Hail")){
+			backgroundImgUrl = "url(img/rain.jpg)";
+			$('#weather-icon').addClass(' wi-rain');
+		} else if(desc.includes("Fog") || desc.includes("Mist") || desc.includes("Haze")){
+			backgroundImgUrl = "url(img/fog.jpg)";
+			$('#weather-icon').addClass(' wi-fog');
+		} else if(desc.includes("Drizzle")){
+			backgroundImgUrl = "url(img/drizzle.jpeg)";
+			$('#weather-icon').addClass(' wi-showers');
+		} else if(desc.includes("Snow") || desc.includes("Ice") || desc.includes("Freezing")){
+			backgroundImgUrl = "url(img/snowy.jpg)";
+			$('#weather-icon').addClass(' wi-snow');
+		} else if(desc.includes("Thunderstorm")){
+			backgroundImgUrl = "url(img/thunderstorm.jpg)";
+			$('#weather-icon').addClass(' wi-thunderstorm');
+		} else{
+			backgroundImgUrl = "url(img/what.png)";
+			$('#weather-icon').addClass(' wi-cloud-refresh');
 		}
 
-
+		$('.background-image').css('background-image', backgroundImgUrl);
 	}
-
+		
 
 });
