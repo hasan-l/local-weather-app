454108393ae7b1f12524b9bea4ceeff9a3ea0d03
Hasan Liaquat <hasan.liaquat@hotmail.com>
2017-08-13 21:32:38 +0100

    implemented rev module pattern and mobile improvements

--- js/main.js
+++ js/main.js
@@ -1,99 +1,110 @@
-$(document).ready(function(){
+var LocalWeatherApp = function(){
 
+	//cached jQuery objects
+	var $expandBtn = $('#expand-btn');
+	var $weatherIcon = $('#weather-icon');
+	var $backgroundImg = $('.background-image');
+	var $tempNum = $('#temp-num');
+	var $tempMeasure = $('#temp-measure');
+
+	$expandBtn.click(expandDiv);
+	
 	function expandDiv(){
-		$('.background-image').toggleClass('changed');
-		$('.main').toggleClass('main-changed');
+		//implements blur on background when div expanded
+		$backgroundImg.toggleClass('changed');
+		//increases container size with a transition
+		$('.weather-container').toggleClass('weather-container-changed');
 		$('#logo').toggleClass('hidden');
 		$('.credits').toggleClass('hidden');
-		$('#weather-icon').toggleClass('hidden');
+		$weatherIcon.toggleClass('hidden');
 
-		//set text to minimise or expand
-		var originalText = $('#expand-btn').text().trim();
+		//set text content of button to minimise if expanded and vice versa
+		var originalText = $expandBtn.text().trim();
 
 		if(originalText === 'Expand'){
-			$('#expand-btn').text('Minimise');
+			$expandBtn.text('Minimise');
 			return;
 		} else {
-            $('#expand-btn').text('Expand');
+            $expandBtn.text('Expand');
         }
 	}//expandDiv
 
-	$('#expand-btn').click(expandDiv);
 
-	//get user location
+	//get user location via AJAX call
 	$.getJSON("https://freegeoip.net/json/", getUserLocation);
-	var city, latitude, longitude;
 
 	function getUserLocation(data){
-		city = data.city;
-		latitude = data.latitude;
-		longitude = data.longitude;
+		var city = data.city;
+		var latitude = data.latitude;
+		var longitude = data.longitude;
+		//inputs 'city, Region' into span, i.e. Slough, England
     	$('#city-name').html(city + ", " + data.region_name);
 
-
     	var wundergroundAPI = 'https://api.wunderground.com/api/1fd8a0bdde10363b/geolookup/conditions/q/' + latitude + ',' + longitude + '.json';
+    	//get and set the weather data via AJAX call after other call is complete (nested callbacks)
 		$.getJSON(wundergroundAPI, setWeatherData);
 
 		//callback
 		function setWeatherData(json){
-			$('#temp-num').html(Math.round(json.current_observation.temp_c));
-			$('#weather-main').html(json.current_observation.weather);
+			$tempNum.html(Math.round(json.current_observation.temp_c));
+			$('#weather-info').html(json.current_observation.weather);
 
 			setBackground(json.current_observation.weather);			
 
-			var tempInCelsius = parseInt($('#temp-num').text(), 10);
+			var tempInCelsius = parseInt($tempNum.text(), 10);
 	
 			//change fron celsius to fahrenheit and back
-			$('#temp-measure').click(function(){
+			$tempMeasure.click(function(){
 
 				var text = $(this).text().trim();
 			
 				if (text === 'C'){
-					$('#temp-measure').html('F');
+					$tempMeasure.html('F');
 					var tempInFah = ((tempInCelsius * 9)/5) + 32;
-					$('#temp-num').html(tempInFah);
+					$tempNum.html(tempInFah);
 				} else if(text === 'F'){
-					$('#temp-measure').html('C');
-					$('#temp-num').html(tempInCelsius);
-					
+					$tempMeasure.html('C');
+					$tempNum.html(tempInCelsius);
 				}
 			});//temp-measure click
 		}//setWeatherData
 	}//getUserLocation
 
-
+	//sets Background image based on description on weather data found in JSON format from API call
 	function setBackground(desc){
 
-		var backgroundImgUrl;
+		var backgroundImgUrl = '';
 
 		if(desc.includes("Cloud") || desc.includes("Overcast")){
 			backgroundImgUrl = "url(img/cloudy.jpg)";
-			$('#weather-icon').addClass(' wi-cloudy');
+			$weatherIcon.addClass(' wi-cloudy');
 		} else if(desc.includes("Clear")){
 			backgroundImgUrl = "url(img/clear.jpg)";
-			$('#weather-icon').addClass(' wi-night-clear');
+			$weatherIcon.addClass(' wi-night-clear');
 		} else if(desc.includes("Rain") || desc.includes("Spray") || desc.includes("Hail")){
 			backgroundImgUrl = "url(img/rain.jpg)";
-			$('#weather-icon').addClass(' wi-rain');
+			$weatherIcon.addClass(' wi-rain');
 		} else if(desc.includes("Fog") || desc.includes("Mist") || desc.includes("Haze")){
 			backgroundImgUrl = "url(img/fog.jpg)";
-			$('#weather-icon').addClass(' wi-fog');
+			$weatherIcon.addClass(' wi-fog');
 		} else if(desc.includes("Drizzle")){
 			backgroundImgUrl = "url(img/drizzle.jpeg)";
-			$('#weather-icon').addClass(' wi-showers');
+			$weatherIcon.addClass(' wi-showers');
 		} else if(desc.includes("Snow") || desc.includes("Ice") || desc.includes("Freezing")){
 			backgroundImgUrl = "url(img/snowy.jpg)";
-			$('#weather-icon').addClass(' wi-snow');
+			$weatherIcon.addClass(' wi-snow');
 		} else if(desc.includes("Thunderstorm")){
 			backgroundImgUrl = "url(img/thunderstorm.jpg)";
-			$('#weather-icon').addClass(' wi-thunderstorm');
+			$weatherIcon.addClass(' wi-thunderstorm');
 		} else{
 			backgroundImgUrl = "url(img/what.png)";
-			$('#weather-icon').addClass(' wi-cloud-refresh');
+			$weatherIcon.addClass(' wi-cloud-refresh');
 		}
 
-		$('.background-image').css('background-image', backgroundImgUrl);
+		$backgroundImg .css('background-image', backgroundImgUrl);
 	}
-		
 
-});
+	return{
+		setBackground: setBackground
+	}
+}();
